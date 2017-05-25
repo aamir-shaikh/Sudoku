12291daccd65a652ef2e622a029dc732119b0598
Aamir Shaikh <aamir.shake93@gmail.com>
2017-05-25 08:25:05 -0700

    Logic behind the game

--- sudoku.py
+++ sudoku.py
@@ -43,3 +43,52 @@ class GameBoard(object):
 			return board
 					
 							
+class Game(object):
+	"""
+	This checks the state of the board and whther the game is complete
+	"""
+	def __init__(self, board_file):
+		self.board_file = board_file
+		self.start_puzzle = GameBoard(board_file).board
+	
+	def start(self):
+		self.game_over = False
+		self.puzzle = []
+		for i in  xrange(9):
+			self.puzzle.append([])
+			for j in xrange(9):
+				self.puzzle[i].append(self.start_puzzle[i][j])
+				
+	def check_for_win(self)
+		for row in xrange(9):
+			if not self.__check_row(row):
+				return False
+		for column in xrange (9):
+			if not self.__check_column(column):
+				return False	
+		for row in xrange(3):
+			for column in xrange(3):
+				if not self.__check_square(row,column):
+					return False
+		self.game_over = True
+		return True
+
+def __check_block(self, block):
+	return set(block) == set(range(1,10))
+
+def __check_row(self, row):
+	return self.__check_block(self.puzzle[row]
+	
+def __check_column(self, column):
+	return self.__check_block([self.puzzle[row][column] for row in xrange(9)])
+	
+def __check_square(self, block):
+	return self.__check_block(
+		[
+			self.puzzle[r][c]
+			for r in xrange(row*3, (row+1) * 3)
+			for c in xrange(column *3, (column +1 *3)
+		]
+	)	
+	
+	
